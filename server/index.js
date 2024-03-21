90358eed383dca381fcc58d684ba87eea3b503d4
Gabriel <gabrielenriquecas@gmail.com>
2024-03-20 20:06:06 -0400

    a

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = createServer(app)
 const io = new Server(server)
 
 io.on('connection', (socket) => {
-    console.log('A user has connected!!!');
+    console.log('A user has connected!!!!');
 
     socket.on('disconnect', () => {
         console.log('user disconnected!');
